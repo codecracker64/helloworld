39f256f259b9e3a6c1a779e0c1366397ba98cdb2
Tarun yadav <48976546+tariqaat@users.noreply.github.com>
2019-04-03 11:14:13 +0530

    Update hellodisplay.c

--- hellodisplay.c
+++ hellodisplay.c
@@ -6,7 +6,7 @@ int main()
    printf("commited by Sourath");
     printf("commited by Arpit");
    printf(" commited by jai mundra");
-      printf(" commited by debol")
-
+      printf(" commited by debol");
+%c
    return 0;
 }
